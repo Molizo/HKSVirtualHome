9c6f040ab334f44287b7ab3ef3a24adfba41258e
Visoiu Mihnea Theodor <visoiu.mihnea@gmail.com>
2018-10-16 08:56:53 +0300

    Added MQTT client functionality

--- HKSVirtualHome/MainWindow.xaml.cs
+++ HKSVirtualHome/MainWindow.xaml.cs
@@ -12,20 +12,56 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MQTTnet;
+using MQTTnet.Client;
 
 namespace HKSVirtualHome
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            var factory = new MqttFactory();
+            var mqttClient = factory.CreateMqttClient();
+            var options = new MqttClientOptionsBuilder()
+                                .WithClientId("HKS Virtual Home Software")
+                                .WithTcpServer("localhost", 8883)
+                                .WithTls()
+                                .WithCleanSession()
+                                .Build();
+
+            mqttClient.Disconnected += async (s, e) =>
+            {
+                Console.WriteLine("[System] Disconnected from Adafruit IO");
+                await Task.Delay(TimeSpan.FromSeconds(5));
+
+                try
+                {
+                    await mqttClient.ConnectAsync(options);
+                }
+                catch
+                {
+                    Console.WriteLine("[System] Connection failed");
+                    Console.WriteLine("[System] Please check your connection and credentials");
+                }
+            };
+            mqttClient.Connected += async (s, e) =>
+            {
+                Console.WriteLine("[System] Connected to server");
+                await mqttClient.SubscribeAsync(new TopicFilterBuilder().WithTopic("/HKS/").Build());
+                Console.WriteLine("[System] Waiting for messages");
+            };
+            mqttClient.ApplicationMessageReceived += (s, e) =>
+            {
+                Console.WriteLine(Encoding.UTF8.GetString(e.ApplicationMessage.Payload));
+            };
+            MqttConnect(mqttClient, options).Wait();
         }
-
-
-
+        private static async Task MqttConnect(IMqttClient mqttClient, IMqttClientOptions options)
+        {
+            Console.WriteLine("[System] Attempting to connect to server...");
+            await mqttClient.ConnectAsync(options);
+        }
     }
 }
