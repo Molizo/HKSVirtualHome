9cadb897a026694554a25ce1a33e316a0aa310b3
Visoiu Mihnea Theodor <visoiu.mihnea@gmail.com>
2018-11-01 14:18:32 +0200

    Updated logic

--- HKSVirtualHome/MainWindow.xaml.cs
+++ HKSVirtualHome/MainWindow.xaml.cs
@@ -22,46 +22,6 @@ namespace HKSVirtualHome
         public MainWindow()
         {
             InitializeComponent();
-            var factory = new MqttFactory();
-            var mqttClient = factory.CreateMqttClient();
-            var options = new MqttClientOptionsBuilder()
-                                .WithClientId("HKS Virtual Home Software")
-                                .WithTcpServer("localhost", 8883)
-                                .WithTls()
-                                .WithCleanSession()
-                                .Build();
-
-            mqttClient.Disconnected += async (s, e) =>
-            {
-                Console.WriteLine("[System] Disconnected from server");
-                await Task.Delay(TimeSpan.FromSeconds(5));
-
-                try
-                {
-                    await mqttClient.ConnectAsync(options);
-                }
-                catch
-                {
-                    Console.WriteLine("[System] Connection failed");
-                    Console.WriteLine("[System] Please check your connection and credentials");
-                }
-            };
-            mqttClient.Connected += async (s, e) =>
-            {
-                Console.WriteLine("[System] Connected to server");
-                await mqttClient.SubscribeAsync(new TopicFilterBuilder().WithTopic("/HKS/").Build());
-                Console.WriteLine("[System] Waiting for messages");
-            };
-            mqttClient.ApplicationMessageReceived += (s, e) =>
-            {
-                Console.WriteLine(Encoding.UTF8.GetString(e.ApplicationMessage.Payload));
-            };
-            MqttConnect(mqttClient, options).Wait();
-        }
-        private static async Task MqttConnect(IMqttClient mqttClient, IMqttClientOptions options)
-        {
-            Console.WriteLine("[System] Attempting to connect to server...");
-            await mqttClient.ConnectAsync(options);
         }
     }
 }
